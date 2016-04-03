21a7df096ac68b2dcc8bcf167b0ff54479918bf9
axelsrz <axel.srz@gmail.com>
2016-04-02 20:34:12 -0600

    Primer commit relevante, se crearon los elementos de la interfaz principal (basico) y se creo el view controller de ajustes, ya se pueden cambiar los nombres y los valores iniciales de las variables mediante el botón de ajustes. Faltan muchos constrains y ajustar el tamaño del controlador.

---
+++ ProyectoFunciones/SettingsViewController.swift
@@ -0,0 +1,81 @@
+//
+//  SettingsViewController.swift
+//  ProyectoFunciones
+//
+//  Created by Axel Suarez on 02/04/16.
+//  Copyright © 2016 Axel Suarez. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+    
+    var nombres : [String: String] = [:]
+    var valores : [String: Int] = [:]
+
+    @IBOutlet var fdVar1: UITextField!
+    @IBOutlet var fdVar2: UITextField!
+    @IBOutlet var fdVar3: UITextField!
+    @IBOutlet var fdVarF1: UITextField!
+    @IBOutlet var fdVarF2: UITextField!
+    @IBOutlet var fdCons1: UITextField!
+    @IBOutlet var fdCons2: UITextField!
+    @IBOutlet var fdCons3: UITextField!
+    @IBOutlet var fdCons4: UITextField!
+    @IBOutlet var fdFuncion: UITextField!
+    @IBOutlet var btGuardar: UIButton!
+    @IBOutlet var btCancelar: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        fdVar1.text = nombres["variable1"]!
+        fdVar2.text = nombres["variable2"]!
+        fdVar3.text = nombres["variable3"]!
+        fdVarF1.text = nombres["varf1"]!
+        fdVarF2.text = nombres["varf2"]!
+        fdFuncion.text = nombres["funcion"]!
+        fdCons1.text = String(valores["constante1"]!)
+        fdCons2.text = String(valores["constante2"]!)
+        fdCons3.text = String(valores["constante3"]!)
+        fdCons4.text = String(valores["constante4"]!)
+        
+        let tap = UITapGestureRecognizer(target: self, action: "quitaTeclado")
+        view.addGestureRecognizer(tap)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func quitaTeclado(){
+        view.endEditing(true)
+    }
+    
+
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if sender as! UIButton == btGuardar{
+            let viewInicial : ViewController = segue.destinationViewController as! ViewController
+            
+            nombres["variable1"] = fdVar1.text!
+            nombres["variable2"] = fdVar2.text!
+            nombres["variable3"] = fdVar3.text!
+            nombres["varf1"] = fdVarF1.text!
+            nombres["varf2"] = fdVarF2.text!
+            nombres["funcion"] = fdFuncion.text!
+            valores["constante1"] = Int(fdCons1.text!)
+            valores["constante2"] = Int(fdCons2.text!)
+            valores["constante3"] = Int(fdCons3.text!)
+            valores["constante4"] = Int(fdCons4.text!)
+            
+            viewInicial.nombres = nombres
+            viewInicial.valores = valores
+        }
+    }
+
+}
--- ProyectoFunciones/ViewController.swift
+++ ProyectoFunciones/ViewController.swift
@@ -9,7 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var nombres : [String: String] = ["variable1" : "var", "variable2" : "var2", "variable3" : "var3", "funcion" : "funcion", "varf1" : "f1", "varf2" : "f2"]
+    var valores : [String: Int] = ["constante1" : 5, "constante2" : 5, "constante3" : 5, "constante4" : 5]
 
+    @IBOutlet var lbFuncion: UILabel!
+    @IBOutlet var lbVarFunc1: UILabel!
+    @IBOutlet var lbVarFunc2: UILabel!
+    @IBOutlet var lbPrimeraAsignacionFuncion: UILabel!
+    @IBOutlet var lbSegundaAsignacionFuncion: UILabel!
+    @IBOutlet var lbResultadoFuncion: UILabel!
+    @IBOutlet var lbVar1: UILabel!
+    @IBOutlet var lbVar2: UILabel!
+    @IBOutlet var lbVar3: UILabel!
+    @IBOutlet var lbPrimeraAsignacionMain: UILabel!
+    @IBOutlet var lbSegundaAsignacionMain: UILabel!
+    @IBOutlet var lbTerceraAsignacionMain: UILabel!
+    @IBOutlet var lbLlamadaAFuncion: UILabel!
+    @IBOutlet var lbParametrosFuncion: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +36,31 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "Ajustes"{
+            let viewAjustes = segue.destinationViewController as! SettingsViewController
+            viewAjustes.nombres = nombres
+            viewAjustes.valores = valores
+        }
+    }
+    
+    @IBAction func unwindAjustes(sender: UIStoryboardSegue) {
+        lbFuncion.text = nombres["funcion"]!
+        lbVarFunc1.text = nombres["varf1"]!
+        lbVarFunc2.text = nombres["varf2"]!
+        lbPrimeraAsignacionFuncion.text = "\(nombres["varf1"]!) = \(nombres["varf1"]!) + \(valores["constante3"]!);"
+        lbSegundaAsignacionFuncion.text = "\(nombres["varf2"]!) = \(nombres["varf2"]!) + \(valores["constante4"]!);"
+        lbResultadoFuncion.text = "result = \(nombres["varf1"]!) + \(nombres["varf2"]!);"
+        lbVar1.text = nombres["variable1"]!
+        lbVar2.text = nombres["variable2"]!
+        lbVar3.text = nombres["variable3"]!
+        lbPrimeraAsignacionMain.text = "\(nombres["variable1"]!) = \(valores["constante1"]!);"
+        lbSegundaAsignacionMain.text = "\(nombres["variable2"]!) = \(valores["constante2"]!);"
+        lbTerceraAsignacionMain.text = "\(nombres["variable3"]!) = ;"
+        lbLlamadaAFuncion.text = nombres["funcion"]!
+        lbParametrosFuncion.text = "(\(nombres["variable1"]!), \(nombres["variable1"]!));"
     }
 
 
