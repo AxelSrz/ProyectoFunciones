fb28b1fb572f7b6ce9e41f28927444928545d195
cesardelabarreda <cesar04_95@hotmail.com>
2016-04-26 22:48:30 -0500

    Se corrigió errores de la secuencia del simulador y error al mostrar result en el simulador

--- ProyectoFunciones/SimulacionViewController.swift
+++ ProyectoFunciones/SimulacionViewController.swift
@@ -115,7 +115,7 @@ class SimulacionViewController: UIViewController {
     
     //si se presiona next la simulacion avanza una vez
     @IBAction func btSiguiente(sender: UIButton) {
-        if instruccion < 8{
+        if instruccion < 9{
             instruccion += 1
         }
         ejecutaInstruccion()
@@ -136,6 +136,25 @@ class SimulacionViewController: UIViewController {
                 UIView.animateWithDuration(0.5, delay: 0, options: .CurveLinear, animations: {
                     self.bgImg.frame = CGRectMake(self.lb01.frame.origin.x,self.lb01.frame.origin.y,self.lb02.frame.origin.x + self.lb02.frame.width - self.lb01.frame.origin.x,self.lb01.frame.height)
                     }, completion: { finished in
+                        self.lbNf1.hidden = true
+                        self.lbNf2.hidden = true
+                        self.lbNvar.hidden = true
+                        self.lbNvar2.hidden = true
+                        self.lbNvar3.hidden = true
+                        self.lbNresult.hidden = true
+                        self.lbF1Value.hidden = true
+                        self.lbF2Value.hidden = true
+                        self.lbVar1Value.hidden = true
+                        self.lbVar2Value.hidden = true
+                        self.lbVar3Value.hidden = true
+                        self.lbResultValue.hidden = true
+                        self.newHighlight = CGRectMake(self.lb01.frame.origin.x,self.lb01.frame.origin.y,self.lb02.frame.origin.x + self.lb02.frame.width - self.lb01.frame.origin.x,self.lb01.frame.height)
+                        self.bgImg.frame = self.newHighlight!
+                })
+            case 1:
+                UIView.animateWithDuration(0.5, delay: 0, options: .CurveLinear, animations: {
+                    self.bgImg.frame = /*CGRectMake(self.lb01.frame.origin.x,self.lb01.frame.origin.y,self.lb02.frame.origin.x + self.lb02.frame.width - self.lb01.frame.origin.x,self.lb01.frame.height)*/CGRectMake(self.lb1.frame.origin.x,self.lb1.frame.origin.y,self.lb1.frame.width,self.lb1.frame.height)
+                    }, completion: { finished in
                         self.lbVar1Value.hidden = false
                         self.lbVar2Value.hidden = false
                         self.lbVar3Value.hidden = false
@@ -155,13 +174,15 @@ class SimulacionViewController: UIViewController {
                         self.lbF1Value.text = ""
                         self.lbF2Value.text = ""
                         self.lbResultValue.text = ""
-                        self.newHighlight = CGRectMake(self.lb01.frame.origin.x,self.lb01.frame.origin.y,self.lb02.frame.origin.x + self.lb02.frame.width - self.lb01.frame.origin.x,self.lb01.frame.height)
+                        //self.newHighlight = CGRectMake(self.lb01.frame.origin.x,self.lb01.frame.origin.y,self.lb02.frame.origin.x + self.lb02.frame.width - self.lb01.frame.origin.x,self.lb01.frame.height)
+                        self.newHighlight = CGRectMake(self.lb1.frame.origin.x,self.lb1.frame.origin.y,self.lb1.frame.width,self.lb1.frame.height)
                         self.bgImg.frame = self.newHighlight!
                         })
 
-            case 1:
+            case 2:
                 UIView.animateWithDuration(0.5, delay: 0, options: .CurveLinear, animations: {
-                    self.bgImg.frame = CGRectMake(self.lb1.frame.origin.x,self.lb1.frame.origin.y,self.lb1.frame.width,self.lb1.frame.height)
+                    //self.bgImg.frame = CGRectMake(self.lb1.frame.origin.x,self.lb1.frame.origin.y,self.lb1.frame.width,self.lb1.frame.height)
+                    self.bgImg.frame = CGRectMake(self.lb2.frame.origin.x,self.lb2.frame.origin.y,self.lb2.frame.width,self.lb2.frame.height)
                     }, completion: { finished in
                         self.lbVar1Value.text = "\(self.c1)"
                         self.lbVar2Value.text = "-"
@@ -169,44 +190,61 @@ class SimulacionViewController: UIViewController {
                         self.lbF1Value.text = ""
                         self.lbF2Value.text = ""
                         self.lbResultValue.text = ""
-                        self.newHighlight = CGRectMake(self.lb1.frame.origin.x,self.lb1.frame.origin.y,self.lb1.frame.width,self.lb1.frame.height)
-                        self.bgImg.frame = self.newHighlight!
-                        })
-            case 2:
-                UIView.animateWithDuration(0.5, delay: 0, options: .CurveLinear, animations: {
-                    self.bgImg.frame = CGRectMake(self.lb2.frame.origin.x,self.lb2.frame.origin.y,self.lb2.frame.width,self.lb2.frame.height)
-                    }, completion: { finished in
-                        self.lbVar1Value.text = "\(self.c1)"
-                        self.lbVar2Value.text = "\(self.c2)"
-                        self.lbVar3Value.text = "-"
-                        self.lbF1Value.text = ""
-                        self.lbF2Value.text = ""
-                        self.lbResultValue.text = ""
+                        //self.newHighlight = CGRectMake(self.lb1.frame.origin.x,self.lb1.frame.origin.y,self.lb1.frame.width,self.lb1.frame.height)
                         self.newHighlight = CGRectMake(self.lb2.frame.origin.x,self.lb2.frame.origin.y,self.lb2.frame.width,self.lb2.frame.height)
                         self.bgImg.frame = self.newHighlight!
                         })
             case 3:
                 UIView.animateWithDuration(0.5, delay: 0, options: .CurveLinear, animations: {
+                    //self.bgImg.frame = CGRectMake(self.lb2.frame.origin.x,self.lb2.frame.origin.y,self.lb2.frame.width,self.lb2.frame.height)
                     self.bgImg.frame = CGRectMake(self.lb31.frame.origin.x,self.lb31.frame.origin.y,self.lb32.frame.origin.x + self.lb32.frame.width - self.lb31.frame.origin.x,self.lb31.frame.height)
+
                     }, completion: { finished in
-                        self.lbNf1.hidden = true
-                        self.lbNf2.hidden = true
-                        self.lbNresult.hidden = true
+                        self.lbResultValue.hidden = true
                         self.lbF1Value.hidden = true
                         self.lbF2Value.hidden = true
-                        self.lbResultValue.hidden = true
+                        self.lbNf2.hidden = true
+                        self.lbNf1.hidden = true
+                        self.lbNresult.hidden = true
                         self.lbVar1Value.text = "\(self.c1)"
                         self.lbVar2Value.text = "\(self.c2)"
                         self.lbVar3Value.text = "-"
                         self.lbF1Value.text = ""
                         self.lbF2Value.text = ""
                         self.lbResultValue.text = ""
+                        //self.newHighlight = CGRectMake(self.lb2.frame.origin.x,self.lb2.frame.origin.y,self.lb2.frame.width,self.lb2.frame.height)
                         self.newHighlight = CGRectMake(self.lb31.frame.origin.x,self.lb31.frame.origin.y,self.lb32.frame.origin.x + self.lb32.frame.width - self.lb31.frame.origin.x,self.lb31.frame.height)
+
                         self.bgImg.frame = self.newHighlight!
                         })
             case 4:
                 UIView.animateWithDuration(0.5, delay: 0, options: .CurveLinear, animations: {
+                    //self.bgImg.frame = CGRectMake(self.lb31.frame.origin.x,self.lb31.frame.origin.y,self.lb32.frame.origin.x + self.lb32.frame.width - self.lb31.frame.origin.x,self.lb31.frame.height)
                     self.bgImg.frame = CGRectMake(self.lb41.frame.origin.x,self.lb41.frame.origin.y,self.lb42.frame.origin.x + self.lb42.frame.width - self.lb41.frame.origin.x,self.lb41.frame.height)
+
+                    }, completion: { finished in
+                        self.lbResultValue.hidden = true
+                        self.lbF1Value.hidden = false
+                        self.lbF2Value.hidden = false
+                        self.lbNf2.hidden = false
+                        self.lbNf1.hidden = false
+                        self.lbNresult.hidden = true
+                        self.lbVar1Value.text = "\(self.c1)"
+                        self.lbVar2Value.text = "\(self.c2)"
+                        self.lbVar3Value.text = "-"
+                        self.lbF1Value.text = "\(self.c1)"
+                        self.lbF2Value.text = "\(self.c2)"
+                        self.lbResultValue.text = "-"
+                        //self.newHighlight = CGRectMake(self.lb31.frame.origin.x,self.lb31.frame.origin.y,self.lb32.frame.origin.x + self.lb32.frame.width - self.lb31.frame.origin.x,self.lb31.frame.height)
+                        self.newHighlight = CGRectMake(self.lb41.frame.origin.x,self.lb41.frame.origin.y,self.lb42.frame.origin.x + self.lb42.frame.width - self.lb41.frame.origin.x,self.lb41.frame.height)
+
+                        self.bgImg.frame = self.newHighlight!
+                        })
+            case 5:
+                UIView.animateWithDuration(0.5, delay: 0, options: .CurveLinear, animations: {
+                    //self.bgImg.frame = CGRectMake(self.lb41.frame.origin.x,self.lb41.frame.origin.y,self.lb42.frame.origin.x + self.lb42.frame.width - self.lb41.frame.origin.x,self.lb41.frame.height)
+                    self.bgImg.frame = CGRectMake(self.lb5.frame.origin.x,self.lb5.frame.origin.y,self.lb5.frame.width,self.lb5.frame.height)
+
                     }, completion: { finished in
                         self.lbResultValue.hidden = false
                         self.lbF1Value.hidden = false
@@ -220,12 +258,16 @@ class SimulacionViewController: UIViewController {
                         self.lbF1Value.text = "\(self.c1)"
                         self.lbF2Value.text = "\(self.c2)"
                         self.lbResultValue.text = "-"
-                        self.newHighlight = CGRectMake(self.lb41.frame.origin.x,self.lb41.frame.origin.y,self.lb42.frame.origin.x + self.lb42.frame.width - self.lb41.frame.origin.x,self.lb41.frame.height)
+                        //self.newHighlight = CGRectMake(self.lb41.frame.origin.x,self.lb41.frame.origin.y,self.lb42.frame.origin.x + self.lb42.frame.width - self.lb41.frame.origin.x,self.lb41.frame.height)
+                        self.newHighlight = CGRectMake(self.lb5.frame.origin.x,self.lb5.frame.origin.y,self.lb5.frame.width,self.lb5.frame.height)
+
                         self.bgImg.frame = self.newHighlight!
                         })
-            case 5:
+            case 6:
                 UIView.animateWithDuration(0.5, delay: 0, options: .CurveLinear, animations: {
-                    self.bgImg.frame = CGRectMake(self.lb5.frame.origin.x,self.lb5.frame.origin.y,self.lb5.frame.width,self.lb5.frame.height)
+                    //self.bgImg.frame = CGRectMake(self.lb5.frame.origin.x,self.lb5.frame.origin.y,self.lb5.frame.width,self.lb5.frame.height)
+                    self.bgImg.frame = CGRectMake(self.lb6.frame.origin.x,self.lb6.frame.origin.y,self.lb6.frame.width,self.lb6.frame.height)
+
                     }, completion: { finished in
                         self.lbVar1Value.text = "\(self.c1)"
                         self.lbVar2Value.text = "\(self.c2)"
@@ -233,12 +275,16 @@ class SimulacionViewController: UIViewController {
                         self.lbF1Value.text = "\(self.c1 + self.cf1)"
                         self.lbF2Value.text = "\(self.c2)"
                         self.lbResultValue.text = "-"
-                        self.newHighlight = CGRectMake(self.lb5.frame.origin.x,self.lb5.frame.origin.y,self.lb5.frame.width,self.lb5.frame.height)
+                        //self.newHighlight = CGRectMake(self.lb5.frame.origin.x,self.lb5.frame.origin.y,self.lb5.frame.width,self.lb5.frame.height)
+                        self.newHighlight = CGRectMake(self.lb6.frame.origin.x,self.lb6.frame.origin.y,self.lb6.frame.width,self.lb6.frame.height)
+
                         self.bgImg.frame = self.newHighlight!
                         })
-            case 6:
+            case 7:
                 UIView.animateWithDuration(0.5, delay: 0, options: .CurveLinear, animations: {
-                    self.bgImg.frame = CGRectMake(self.lb6.frame.origin.x,self.lb6.frame.origin.y,self.lb6.frame.width,self.lb6.frame.height)
+                    //self.bgImg.frame = CGRectMake(self.lb6.frame.origin.x,self.lb6.frame.origin.y,self.lb6.frame.width,self.lb6.frame.height)
+                    self.bgImg.frame = CGRectMake(self.lb7.frame.origin.x,self.lb7.frame.origin.y,self.lb7.frame.width,self.lb7.frame.height)
+
                     }, completion: { finished in
                         self.lbVar1Value.text = "\(self.c1)"
                         self.lbVar2Value.text = "\(self.c2)"
@@ -246,12 +292,16 @@ class SimulacionViewController: UIViewController {
                         self.lbF1Value.text = "\(self.c1 + self.cf1)"
                         self.lbF2Value.text = "\(self.c2 + self.cf2)"
                         self.lbResultValue.text = "-"
-                        self.newHighlight = CGRectMake(self.lb6.frame.origin.x,self.lb6.frame.origin.y,self.lb6.frame.width,self.lb6.frame.height)
+                        //self.newHighlight = CGRectMake(self.lb6.frame.origin.x,self.lb6.frame.origin.y,self.lb6.frame.width,self.lb6.frame.height)
+                        self.newHighlight = CGRectMake(self.lb7.frame.origin.x,self.lb7.frame.origin.y,self.lb7.frame.width,self.lb7.frame.height)
+
                         self.bgImg.frame = self.newHighlight!
                         })
-            case 7:
+            case 8:
                 UIView.animateWithDuration(0.5, delay: 0, options: .CurveLinear, animations: {
-                    self.bgImg.frame = CGRectMake(self.lb7.frame.origin.x,self.lb7.frame.origin.y,self.lb7.frame.width,self.lb7.frame.height)
+                    //self.bgImg.frame = CGRectMake(self.lb7.frame.origin.x,self.lb7.frame.origin.y,self.lb7.frame.width,self.lb7.frame.height)
+                    self.bgImg.frame = CGRectMake(self.lb81.frame.origin.x,self.lb81.frame.origin.y,self.lb82.frame.origin.x + self.lb82.frame.width - self.lb81.frame.origin.x,self.lb81.frame.height)
+
                     }, completion: { finished in
                         self.lbVar1Value.text = "\(self.c1)"
                         self.lbVar2Value.text = "\(self.c2)"
@@ -259,12 +309,14 @@ class SimulacionViewController: UIViewController {
                         self.lbF1Value.text = "\(self.c1 + self.cf1)"
                         self.lbF2Value.text = "\(self.c2 + self.cf2)"
                         self.lbResultValue.text = "\(self.c1 + self.cf1 + self.c2 + self.cf2)"
-                        self.newHighlight = CGRectMake(self.lb7.frame.origin.x,self.lb7.frame.origin.y,self.lb7.frame.width,self.lb7.frame.height)
+                        //self.newHighlight = CGRectMake(self.lb7.frame.origin.x,self.lb7.frame.origin.y,self.lb7.frame.width,self.lb7.frame.height)
+                        self.newHighlight = CGRectMake(self.lb81.frame.origin.x,self.lb81.frame.origin.y,self.lb82.frame.origin.x + self.lb82.frame.width - self.lb81.frame.origin.x,self.lb81.frame.height)
+
                         self.bgImg.frame = self.newHighlight!
                         })
-            case 8:
+            case 9:
                 UIView.animateWithDuration(0.5, delay: 0, options: .CurveLinear, animations: {
-                    self.bgImg.frame = CGRectMake(self.lb81.frame.origin.x,self.lb81.frame.origin.y,self.lb82.frame.origin.x + self.lb82.frame.width - self.lb81.frame.origin.x,self.lb81.frame.height)
+                    //self.bgImg.frame = CGRectMake(self.lb81.frame.origin.x,self.lb81.frame.origin.y,self.lb82.frame.origin.x + self.lb82.frame.width - self.lb81.frame.origin.x,self.lb81.frame.height)
                     }, completion: { finished in
                         self.lbVar1Value.text = "\(self.c1)"
                         self.lbVar2Value.text = "\(self.c2)"
@@ -272,8 +324,8 @@ class SimulacionViewController: UIViewController {
                         self.lbF1Value.text = "\(self.c1 + self.cf1)"
                         self.lbF2Value.text = "\(self.c2 + self.cf2)"
                         self.lbResultValue.text = "\(self.c1 + self.cf1 + self.c2 + self.cf2)"
-                        self.newHighlight = CGRectMake(self.lb81.frame.origin.x,self.lb81.frame.origin.y,self.lb82.frame.origin.x + self.lb82.frame.width - self.lb81.frame.origin.x,self.lb81.frame.height)
-                        self.bgImg.frame = self.newHighlight!
+                        //self.newHighlight = CGRectMake(self.lb81.frame.origin.x,self.lb81.frame.origin.y,self.lb82.frame.origin.x + self.lb82.frame.width - self.lb81.frame.origin.x,self.lb81.frame.height)
+                        //self.bgImg.frame = self.newHighlight!
                         })
             
             default: break
